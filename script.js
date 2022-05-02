e99d2049fc6348e40f8ae4ad0bf63a7ff4ea2c30
christinamdo <104749632+christinamdo@users.noreply.github.com>
2022-05-02 13:07:53 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+(function(){
+    var words = [
+        ' designs',
+        ' illustrates',
+        ' creates',
+        
+        ], i = 0;
+    setInterval(function(){
+        $('#changingword').fadeOut(function(){
+            $(this).html(words[i=(i+1)%words.length]).fadeIn();
+        });
+    }, 3000);
+      
+})();
+
